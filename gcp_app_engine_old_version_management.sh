85248f6617ddca3c38888b560bed15d130747ce0
anand.gopinath <anand.gopinath@qburst.com>
2024-05-17 15:44:24 +0530

    ENG-1023

--- gcp_app_engine_old_version_management.sh
+++ gcp_app_engine_old_version_management.sh
@@ -3,52 +3,49 @@
 # Get the project ID
 PROJECT_ID=$(gcloud config get-value project)
 
-# Get all services
-SERVICES=$(gcloud app services list --project $PROJECT_ID --format 'value(id)' | grep -v default)
+# Get the service name from the environment variable
+SERVICE_ID=$SERVICE_NAME
 
-for SERVICE_ID in $SERVICES
-do
-  echo 
-  echo "Service: $SERVICE_ID"
+echo 
+echo "Service: $SERVICE_ID"
 
-  # Get the latest version based on time of creation with 100% traffic
-  LATEST_VERSION=$(gcloud app versions list --project $PROJECT_ID --service $SERVICE_ID --filter="traffic_split=1" --sort-by '~creation_time' --format 'value(id)' --limit 1)
+# Get the latest version based on time of creation with 100% traffic
+LATEST_VERSION=$(gcloud app versions list --project $PROJECT_ID --service $SERVICE_ID --filter="traffic_split=1" --sort-by '~creation_time' --format 'value(id)' --limit 1)
 
-  # If there is no version with 100% traffic, skip to the next service
-  if [[ -z "$LATEST_VERSION" ]]; then
-    echo
-    echo "No version with 100% traffic for service $SERVICE_ID"
-    continue
-  fi
-
-  # Print the ID of the latest version
+# If there is no version with 100% traffic, print a message and exit
+if [[ -z "$LATEST_VERSION" ]]; then
   echo
-  echo "Latest Version: $LATEST_VERSION"
+  echo "No version with 100% traffic for service $SERVICE_ID"
+  exit 1
+fi
 
-  echo
-  echo "Old Versions:"
-  # List all versions with 0% traffic that are not the latest version
-  OLD_VERSIONS=$(gcloud app versions list --project $PROJECT_ID --service $SERVICE_ID --filter="traffic_split=0 AND id != $LATEST_VERSION" --format="value(id)")
+# Print the ID of the latest version
+echo
+echo "Latest Version: $LATEST_VERSION"
 
-  # Check if there are old versions
-  if [[ -z "$OLD_VERSIONS" ]]; then
-    echo "No old versions found"
-  else
-    # Print the IDs of the old versions line by line
-    for OLD_VERSION in $OLD_VERSIONS
-    do
-      echo $OLD_VERSION
-    done
-  fi
+echo
+echo "Old Versions:"
+# List all versions with 0% traffic that are not the latest version
+OLD_VERSIONS=$(gcloud app versions list --project $PROJECT_ID --service $SERVICE_ID --filter="traffic_split=0 AND id != $LATEST_VERSION" --format="value(id)")
 
-  # Stop the old versions and print the IDs of the stopped versions
+# Check if there are old versions
+if [[ -z "$OLD_VERSIONS" ]]; then
+  echo "No old versions found"
+else
+  # Print the IDs of the old versions line by line
   for OLD_VERSION in $OLD_VERSIONS
   do
-    echo "Stopping version: $OLD_VERSION"
-    gcloud app versions stop --project $PROJECT_ID --service $SERVICE_ID $OLD_VERSION --quiet
+    echo $OLD_VERSION
   done
+fi
 
-  # Add two line spaces
-  echo
-  echo
+# Stop the old versions and print the IDs of the stopped versions
+for OLD_VERSION in $OLD_VERSIONS
+do
+  echo "Stopping version: $OLD_VERSION"
+  gcloud app versions stop --project $PROJECT_ID --service $SERVICE_ID $OLD_VERSION --quiet
 done
+
+# Add two line spaces
+echo
+echo
